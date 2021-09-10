635996fbb03096cab8d0645d5a6302e451a15c9e
Nosmas <sam.tory@hotmail.com>
2021-09-10 14:58:11 +1000

    Added bomb functionality and display

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
+import random
+
+
+
+
+
+
+
+
+
 def new_layout():
-    row1 = ["X","X","X","X","X"]
-    row2 = ["X","X","X","X","X"]
-    row3 = ["X","X","X","X","X"]
     row4 = ["X","X","X","X","X"]
-    row5 = ["X","X","X","X","X"]
-    row_layout = [row1,row2,row3,row4,row5]
-
-    column1 = []
-    column2 = []
-    column3 = []
-    column4 = []
-    column5 = []
-    column_layout = [column1,column2,column3,column4,column5]
+    row3 = ["X","X","X","X","X"]
+    row2 = ["X","X","X","X","X"]
+    row1 = ["X","X","X","X","X"]
+    row0 = ["X","X","X","X","X"]
 
 
-    
+
+    base_layout = [row0,row1,row2,row3,row4]
+    display_base = []
+    bomb_count = 0
+    current_bombs = []
+
+    while bomb_count <= 3:
+        row_bomb = random.randint(0,4)
+        column_bomb = random.randint(0,4)
+        base_layout[row_bomb][column_bomb] = "O"
+        bomb_count += 1
+        bomb_placement = [column_bomb,row_bomb]
+        current_bombs.append(bomb_placement)
+
+
+    for i in base_layout[::-1]:
+        print(i)
+    print(current_bombs)
+
+
+
+
+
+
+
 
 
 new_layout()
