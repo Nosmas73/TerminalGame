acdc5fd5daf1445ddbcb2905d9785f612249f45f
Nosmas <sam.tory@hotmail.com>
2021-09-10 13:55:26 +1000

    basic layout idea

---
+++ main.py
@@ -0,0 +1,20 @@
+def new_layout():
+    row1 = ["X","X","X","X","X"]
+    row2 = ["X","X","X","X","X"]
+    row3 = ["X","X","X","X","X"]
+    row4 = ["X","X","X","X","X"]
+    row5 = ["X","X","X","X","X"]
+    row_layout = [row1,row2,row3,row4,row5]
+
+    column1 = []
+    column2 = []
+    column3 = []
+    column4 = []
+    column5 = []
+    column_layout = [column1,column2,column3,column4,column5]
+
+
+    
+
+
+new_layout()
